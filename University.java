79b8c5681c375460624c0e6879572ccf03d0e3a9
eainoon <eainans0107@gmail.com>
2025-04-15 21:13:11 +0800

    University.java

--- University.java
+++ University.java
@@ -1,10 +1,13 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class University {  
     private String name;
     private String location;
     private int totalStudents;
     private int ranking;
-    private String faculties;
-    private String programmes;
+    private List<String> faculties;
+    private List<String> programmes;
 
     // Default Constructor
     public University() {
@@ -12,12 +15,12 @@ public class University {
         this.location = "Shah Alam, Selangor, Malaysia";
         this.totalStudents = 17000;
         this.ranking = 580;
-        this.faculties = "FISE, FBMP, FHLS, IMS, SHCA, SESS";
-        this.programmes = "BCS, BCF, BBC, BGD, BICT";
+        this.faculties = Arrays.asList("FISE", "FBMP", "FHLS", "IMS", "SHCA", "SESS");
+        this.programmes = Arrays.asList("BCS", "BCF", "BBC", "BGD", "BICT");
     }
 
     // Parameterized Constructor
-    public University(String name, String location, int totalStudents, int ranking, String faculties, String programmes) {
+    public University(String name, String location, int totalStudents, int ranking, List<String> faculties, List<String> programmes) {
         this.name = name;
         this.location = location;
         this.totalStudents = totalStudents;
@@ -26,37 +29,43 @@ public class University {
         this.programmes = programmes;
     }
 
-    // Getter Methods
+    // Getters
     public String getName() { return name; }
     public String getLocation() { return location; }
     public int getTotalStudents() { return totalStudents; }
     public int getRanking() { return ranking; }
-    public String getFaculties() { return faculties; }
-    public String getProgrammes() { return programmes; }
+    public List<String> getFaculties() { return faculties; }
+    public List<String> getProgrammes() { return programmes; }
 
-    // Setter Methods
+    // Setters
     public void setName(String name) { this.name = name; }
     public void setLocation(String location) { this.location = location; }
     public void setTotalStudents(int totalStudents) { this.totalStudents = totalStudents; }
     public void setRanking(int ranking) { this.ranking = ranking; }
-    public void setFaculties(String faculties) { this.faculties = faculties; }
-    public void setProgrammes(String programmes) { this.programmes = programmes; }
+    public void setFaculties(List<String> faculties) { this.faculties = faculties; }
+    public void setProgrammes(List<String> programmes) { this.programmes = programmes; }
 
     // Method 1 - Display total students
     public void displayTotalStudents() {
-        System.out.println("The total number of students in the university is " + totalStudents);
+        System.out.println("Total students in the university: " + totalStudents);
     }
 
     // Method 2 - List faculties
     public void listFaculties() {
-        System.out.println("The available faculties in the university are: " + faculties);
+        System.out.println("Faculties available: " + String.join(", ", faculties));
     }
 
-    // Method 3 - Display university name and location
+    // Method 3 - List programmes
+    public void listProgrammes() {
+        System.out.println("Programmes offered: " + String.join(", ", programmes));
+    }
+
+    // Method 4 - Display university name and location
     public void displayUniversityDetails() {
-        System.out.println("== University Details ==");
+        System.out.println("\n== University Details ==");
         System.out.println("Name: " + name);
         System.out.println("Location: " + location);
         System.out.println("Ranking: " + ranking);
     }
 }
+//In Assignment 2, Marsha is continuing this from Marsha in Assignment 1
