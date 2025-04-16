ff79a30cdeff26ce0a4e0bca586cada47740ad32
eainoon <eainans0107@gmail.com>
2025-04-16 20:44:14 +0800

    University.java

--- University.java
+++ University.java
@@ -1,72 +1,82 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+// University class stores details about the university and manages faculties, courses, clubs, etc.
+public class University {
 
-public class University {  
     private String name;
     private String location;
+    private String state;
     private int totalStudents;
-    private int ranking;
     private List<String> faculties;
-    private List<String> programmes;
+    private List<String> courses;
+    private List<Department> departments;
+    private Library library;
+    private List<Club> clubs;
+    private List<Student> students;
 
-    // Default Constructor
-    public University() {
-        this.name = "MSU";
-        this.location = "Shah Alam, Selangor, Malaysia";
-        this.totalStudents = 17000;
-        this.ranking = 580;
-        this.faculties = new ArrayList<>(Arrays.asList("FISE", "FBMP", "FHLS", "IMS", "SHCA", "SESS"));
-        this.programmes = new ArrayList<>(Arrays.asList("BCS", "BCF", "BBC", "BGD", "BICT"));
-    }
-
-    // Parameterized Constructor
-    public University(String name, String location, int totalStudents, int ranking, List<String> faculties, List<String> programmes) {
+    // Constructor to initialize university details
+    public University(String name, String location, String state, int totalStudents) {
         this.name = name;
         this.location = location;
+        this.state = state;
         this.totalStudents = totalStudents;
-        this.ranking = ranking;
-        this.faculties = faculties;
-        this.programmes = programmes;
+        this.faculties = new ArrayList<>();
+        this.courses = new ArrayList<>();
+        this.departments = new ArrayList<>();
+        this.clubs = new ArrayList<>();
+        this.students = new ArrayList<>();
     }
 
-    // Getters
-    public String getName() { return name; }
-    public String getLocation() { return location; }
-    public int getTotalStudents() { return totalStudents; }
-    public int getRanking() { return ranking; }
-    public List<String> getFaculties() { return faculties; }
-    public List<String> getProgrammes() { return programmes; }
-
-    // Setters
-    public void setName(String name) { this.name = name; }
-    public void setLocation(String location) { this.location = location; }
-    public void setTotalStudents(int totalStudents) { this.totalStudents = totalStudents; }
-    public void setRanking(int ranking) { this.ranking = ranking; }
-    public void setFaculties(List<String> faculties) { this.faculties = faculties; }
-    public void setProgrammes(List<String> programmes) { this.programmes = programmes; }
-
-    // Method 1 - Display total students
-    public void displayTotalStudents() {
-        System.out.println("The total number of students in the university is " + totalStudents);
+    // Method to add a faculty to the university
+    public void addFaculty(String faculty) {
+        faculties.add(faculty);
     }
 
-    // Method 2 - List faculties
-    public void listFaculties() {
-        System.out.println("The available faculties in the university are: " + String.join(", ", faculties));
+    // Method to add a course to the university
+    public void addCourse(String course) {
+        courses.add(course);
     }
 
-    // Method 3 - List programmes
-    public void listProgrammes() {
-        System.out.println("The programmes offered here are: " + String.join(", ", programmes));
+    // Method to add a department to the university
+    public void addDepartment(Department department) {
+        departments.add(department);
     }
 
-    // Method 4 - Display university name and location
+    // Method to set the library for the university
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    // Method to add a club to the university
+    public void addClub(Club club) {
+        clubs.add(club);
+    }
+
+    // Method to add a student to the university
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    // Method to display all university details
     public void displayUniversityDetails() {
-        System.out.println("== University Details ==");
-        System.out.println("Name: " + name);
-        System.out.println("Location: " + location);
-        System.out.println("Ranking: " + ranking);
+        System.out.println("University Name: " + name);
+        System.out.println("Location: " + location + ", " + state);
+        System.out.println("Total Students: " + totalStudents);
+    }
+
+    // Method to display all faculties
+    public void displayFaculties() {
+        System.out.println("\nFaculties:");
+        for (String faculty : faculties) {
+            System.out.println("- " + faculty);
+        }
+    }
+
+    // Method to display all courses
+    public void displayCourses() {
+        System.out.println("\nCourses Offered:");
+        for (String course : courses) {
+            System.out.println("- " + course);
+        }
     }
 }
+
 //In Assignment 2, Marsha is continuing this from Marsha in Assignment 1
