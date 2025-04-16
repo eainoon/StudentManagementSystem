461e32cb151c41518cd80a2622ff3b202e777fd8
eainoon <eainans0107@gmail.com>
2025-04-16 20:47:06 +0800

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,34 +1,30 @@
+// This class represents a Library in the university system
 public class Library {
-    private String location;
-    private int totalBooks;
-    private boolean isOpen;
+    private String name;
+    private String openingHours;
 
-    // Constructor
-    public Library(String location, int totalBooks, boolean isOpen) {
-        this.location = location;
-        this.totalBooks = totalBooks;
-        this.isOpen = isOpen;
+    // Constructor to initialize the Library's name and opening hours
+    public Library(String name, String openingHours) {
+        this.name = name;
+        this.openingHours = openingHours;
     }
 
-    // Methods
-    public void borrowBook() {
-        if (totalBooks > 0 && isOpen) {
-            totalBooks--;
-            System.out.println("A book has been borrowed. Remaining books: " + totalBooks);
-        } else {
-            System.out.println("Cannot borrow books right now.");
-        }
+    // Getter for Library's name
+    public String getName() {
+        return name;
     }
 
-    public void returnBook() {
-        totalBooks++;
-        System.out.println("A book was returned. Total books: " + totalBooks);
+    // Getter for Library's opening hours
+    public String getOpeningHours() {
+        return openingHours;
     }
 
-    public void status() {
-        System.out.println("Library Location: " + location);
-        System.out.println("Open Status: " + (isOpen ? "Open" : "Closed"));
-        System.out.println("Available Books: " + totalBooks);
+    // Method to display the Library's information
+    public void displayLibraryInfo() {
+        System.out.println("\nLibrary Info:");
+        System.out.println("Name: " + name);
+        System.out.println("Opening Hours: " + openingHours);
     }
 }
+
 //Eainan is doing this in Assignment 2
